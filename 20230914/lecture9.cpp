7d784ff6f5e7b04cbc041674306aeab92ce854df
treetleeo <treetleeo@gmail.com>
2023-09-14 11:58:43 +0900

    for

---
+++ 20230914/lecture9.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int i;
+	int sum = 0;
+	int val = 1;
+	for (i = 0; i < 10; i++) {
+		printf("i = %d val = %d sum = %d\n", i, val, sum);
+		sum += val;
+		val += 1;
+	}
+
+	printf("sum = %d\n", sum);
+	return 0;
+}
