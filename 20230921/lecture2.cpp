cbb60241d9be7d52717d45ee4cfff86352cfb6a7
treetleeo <treetleeo@gmail.com>
2023-09-21 11:26:36 +0900

    Update lecture2.cpp

--- 20230921/lecture2.cpp
+++ 20230921/lecture2.cpp
@@ -1,7 +1,23 @@
 #include <stdio.h>
-void calculator(int selector)
+void sum(double a, double b) {
+	double result = a + b;
+	printf("%lf + %lf : %lf", a, b, result);
+}
+void sub(double a, double b) {
+	double result = a - b;
+	printf("%lf - %lf : %lf", a, b, result);
+}
+void mul(double a, double b)
 {
-	double num1, num2;
+	double result = a * b;
+	printf("%lf * %lf : %lf", a, b, result);
+}
+void div(double a, double b) {
+	double result = a / b;
+	printf("%lf / %lf : %lf", a, b, result);
+}
+void calculator(int selector, int num1, int num2)
+{
 	printf("숫자 1을 입력하시오 : ");
 	scanf_s("%lf", &num1);
 
@@ -9,14 +25,14 @@ void calculator(int selector)
 	scanf_s("%lf", &num2);
 
 	if (selector == 1) sum(num1, num2);
-	elseif(selector == 2) sub(num1, num2);
+	else if(selector == 2) sub(num1, num2);
 	else if (selector == 3) mul(num1, num2);
 	else if (selector == 4) div(num1, num2);
 }
 int main()
 {
-	int sel;
+	int sel, num1, num2;
 	printf("1.더하기 2.빼기 3.곱하기 4.나누기 : ");
 	scanf_s("%d", &sel);
-	calculator(sel);
+	calculator(sel, num1, num2);
 }
